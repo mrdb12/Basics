cce03bda1c950ba5e6ea7f379da23b845add1e0f
mrdb12 <dbruchwald@gmail.com>
2021-09-20 14:53:00 +0200

    math library

--- mathlib.c
+++ mathlib.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+
+int main(void){
+  int number;
+  double sqrtnum;
+  double e;
+
+  scanf("%d", &number);
+
+  sqrtnum = sqrt(number);
+  printf("%.8f\n", sqrtnum);
+
+  e = exp(1);
+  printf("%.10f\n", e);
+
+  return 0;
+}
